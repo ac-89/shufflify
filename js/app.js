2fba89155145d9d1e5b1e28889c7e8e4c246df55
ac-89 <alancarroll89@gmail.com>
2023-05-21 13:01:14 +0100

    Additional styling. Media Queries.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const bWard = document.querySelector("fa-stpe-backward");
+const fWard = document.querySelector("fa-step-forward");
+
 fetch(
   "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&formatversion=2&rnlimit=5&rnnamespace=0"
 )
@@ -5,20 +8,21 @@ fetch(
   .then((data) => {
     let artist = document.querySelector(".artist");
     let article = data.query.random[0].title;
-    let brIndex = article.indexOf("(");
-    let trimmed = article.slice(0, brIndex);
-    console.log(trimmed, article);
+    let brIndex = () => {
+      if (article.split("").includes("(")) {
+        return article.indexOf("(");
+      }
+    };
+    let trimmed = article.slice(0, brIndex());
+    console.log(trimmed, article, brIndex());
     artist.innerText = trimmed;
   });
 
-fetch("https://api.quotable.io/quotes/random?maxLength=30")
+fetch("https://api.quotable.io/quotes/random?maxLength=35")
   .then((res) => res.json())
   .then((data) => {
-    // let artist = document.querySelector(".artist");
-    // artist.innerText = data.query.random[0].title;
     let quote = data[0].content;
     console.log(quote);
     let title = document.querySelector(".song-title");
     title.innerText = quote;
-    // let section = console.log(endIndex);
   });
